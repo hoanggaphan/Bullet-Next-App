4554f30bcde6afbb9cdbdf765cf7bcc95fee3070
hoanggaphan <phansihoang1998@gmail.com>
2020-10-10 13:46:15 +0700

    e2e-testing-with-cypress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,13 @@
+const { promises: fsPromises } = require("fs");
+const os = require("os");
+const path = require("path");
+
+module.exports = (on, config) => {
+  on("task", {
+    async resetData() {
+      const dataDir = path.join(os.tmpdir(), "bulletproof-next-app");
+      await fsPromises.rmdir(dataDir, { recursive: true });
+      return null;
+    },
+  });
+};
